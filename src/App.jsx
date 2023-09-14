952a9bcfa705bc72fc01ba71554797ab4a02a5af
sadia11-a <sadiasorna96@gmail.com>
2023-09-14 19:59:23 +0600

    here start my assignment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-[#1C1B1B] text-3xl font-bold mt-8 mb-6">
+        Course Registration
+      </h1>
+    </>
+  );
+}
+
+export default App;
