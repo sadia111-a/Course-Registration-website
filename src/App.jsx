10ba4018361bf39c79cbffc108ac4186f84b77d4
sadia11-a <sadiasorna96@gmail.com>
2023-09-15 00:41:39 +0600

    all the cards done with fetching from my fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import Cart from "./Components/Cart";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-center text-[#1C1B1B] text-3xl font-bold mt-8 mb-6">
+      <h1 className="text-center text-[#1C1B1B] text-xl md:text-xl lg:text-3xl font-bold mt-8 mb-6">
         Course Registration
       </h1>
+      <div className="flex flex-col md:flex-col lg:flex-row">
+        <Home></Home>
+        <Cart></Cart>
+      </div>
     </>
   );
 }
---
+++ src/Components/Cart.jsx
@@ -0,0 +1,11 @@
+const Cart = () => {
+  return (
+    <div className="w-full lg:w-1/4">
+      <h2 className="text-[#1C1B1B] text-xl font-bold text-center">
+        Course Name
+      </h2>
+    </div>
+  );
+};
+
+export default Cart;
